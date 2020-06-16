b209eaef7fd233cb9b2f42cdd8dd9d1be9cb5bb8
Sammar Javed <sammarjaved49@gmail.com>
2020-06-16 18:03:52 +0200

    Added route and Controller

---
+++ app/Http/Controllers/MovieTitlesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * Class MovieTitlesController
+ */
+class MovieTitlesController extends Controller
+{
+    /**
+     * Get Details about Tv show for given title.
+     */
+    public function getTvShowByTitle()
+    {
+    }
+}
